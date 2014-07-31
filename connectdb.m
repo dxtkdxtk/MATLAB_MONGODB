49a99190b346b6edda95434f7d3d81c2e734a9cf
jebin <cepinlin@gmail.com>
2014-07-31 16:06:06 +0800

    fix varargin index

--- connectdb.m
+++ connectdb.m
@@ -1,12 +1,12 @@
 function connectdb(varargin)
-%MCONNECT 连接数据库
+%CONNECTDB 连接数据库
 server = 'localhost';
 database = 'MarketData';
 if(nargin == 1)
     server = varargin{1};
 elseif(nargin == 2)
     server = varargin{1};
-    database = varargin{1};
+    database = varargin{2};
 elseif(nargin > 2)
     error('参数过多');
 end
